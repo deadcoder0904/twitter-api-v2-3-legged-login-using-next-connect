b60cce15840ad978509451ae669e106ca0c1fc75
Akshay Kadam <akshaykadam0904@gmail.com>
2021-09-24 12:30:16 +0530

    Stop opening authWindow for now

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,14 +1,14 @@
 const twitterLogin = async () => {
-  const authWindow = window.open(
-    "about:blank",
-    "_blank",
-    "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600,left=100,top=100"
-  );
+  // const authWindow = window.open(
+  //   "about:blank",
+  //   "_blank",
+  //   "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600,left=100,top=100"
+  // );
 
-  if (!authWindow) {
-    console.error("Your browser is blocking popups. Allow popups log in with Twitter.");
-    return;
-  }
+  // if (!authWindow) {
+  //   console.error("Your browser is blocking popups. Allow popups log in with Twitter.");
+  //   return;
+  // }
 
   const res = await fetch('/api/twitter/generate-auth-link');
 	console.log({res})
