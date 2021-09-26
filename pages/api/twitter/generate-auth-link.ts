d42efe035c2e89644bc7066746a5061f7630b474
Akshay Kadam <akshaykadam0904@gmail.com>
2021-09-26 12:47:27 +0530

    Remove redundant code

--- pages/api/twitter/generate-auth-link.ts
+++ pages/api/twitter/generate-auth-link.ts
@@ -2,16 +2,9 @@ import { NextApiResponse } from 'next'
 import TwitterApi from 'twitter-api-v2'
 
 import { TWITTER_CONFIG } from '../../../lib/config'
-import { IS_PRODUCTION } from '../../../utils/index'
+import { SERVER_URL } from '../../../utils/index'
 import { NextIronRequest } from '../../../types/index'
 import handler from '../../../server/api-route'
-
-export const CLIENT_URL = IS_PRODUCTION
-  ? 'https://mysite.com/'
-  : 'http://localhost:3000/'
-export const SERVER_URL = IS_PRODUCTION
-  ? 'https://mysite.com/api/'
-  : 'http://localhost:3000/'
 
 const generateAuthLink = async (
   req: NextIronRequest,
