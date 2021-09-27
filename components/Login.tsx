f16b011a5b88fd45bea6d3113a387b559b49b8a2
Akshay Kadam <akshaykadam0904@gmail.com>
2021-09-27 21:36:36 +0530

    Use router.push

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,30 +1,42 @@
-const twitterLogin = async () => {
-  const authWindow = window.open(
-    "about:blank",
-    "_blank",
-    "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600,left=100,top=100"
-  );
+import Router, { useRouter } from 'next/router'
 
-  if (!authWindow) {
-    console.error("Your browser is blocking popups. Allow popups log in with Twitter.");
-    return;
+
+export const Login = () => {
+  const router = useRouter()
+  
+  const twitterLogin = async () => {
+    const authWindow = window.open(
+      'about:blank',
+      '_blank',
+      'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=800,height=600,left=100,top=100'
+    )
+  
+    if (!authWindow) {
+      console.error(
+        'Your browser is blocking popups. Allow popups log in with Twitter.'
+      )
+      return
+    }
+  
+    const res = await fetch(`/api/twitter/generate-auth-link`)
+    const data: { redirect: string } = await res.json()
+  
+    const authURL = data.redirect
+    authWindow.location.href = authURL
+  
+    // listen for "window.opener.postMessage" sent from backend via <script>
+    window.addEventListener('message', (event) => {
+      if (event?.data?.username) {
+        const username = event?.data?.username
+        router.push(`/app?username=${username}`)
+      }
+    })
   }
 
-  const res = await fetch(`/api/twitter/generate-auth-link`);
-  const data: {redirect: string} = await res.json()
-  
-	const authURL = data.redirect;
-  authWindow.location.href = authURL;
 
-  // listen for "window.opener.postMessage" sent from backend via <script>
-  window.addEventListener("message", (event) => {
-    if (event?.data?.username) {
-      const username = event?.data?.username;
-      window.location.href = `/app?username=${username}`
-    }
-  });
+  return (
+    <>
+      <button onClick={twitterLogin}>Login with Twitter</button>
+    </>
+  )
 }
-
-export const Login = () => <>
-  <button onClick={twitterLogin}>Login with Twitter</button>
-</>
